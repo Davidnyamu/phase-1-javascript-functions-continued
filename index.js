f8ad21cec597ccddfe7752d7a98ef4eaf9d50d2d
+ <+>
2024-03-27 15:44:09 +0300

    function-contined

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+// Example usage:
+console.log(saturdayFun());  // Output: This Saturday, let's roller-skate!
+console.log(saturdayFun('bathe my dog'));  // Output: This Saturday, I want to roller-skate.
+
+console.log(mondayWork());  // Output: This Monday, I have to go to the office.
+console.log(mondayWork('work remotely'));  // Output: This Monday, I will work remotely,
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction());  // Output: You are *special*!!!
+console.log(encouragingPromptFunction("awesome"));  // Output: You are *awesome*!!!
+
+
+
+
